3bd378f90c950f9868f015c23868d78f51611b7d
Wayn Gorold <wgorold@e6r1p6.21-school.ru>
2019-06-04 19:48:05 +0300

    %d

--- precision/ft_make_d.c
+++ precision/ft_make_d.c
@@ -6,13 +6,13 @@
 /*   By: wgorold <wgorold@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/30 15:36:58 by wgorold           #+#    #+#             */
-/*   Updated: 2019/06/04 19:33:33 by wgorold          ###   ########.fr       */
+/*   Updated: 2019/06/04 19:47:41 by wgorold          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "longd.h"
 
-void	putpres(char *str, int len, int len_num)
+void	putpreci(char *str, int len, int len_num)
 {
 	fill('0', len - len_num);
 	ft_putstr(str);
@@ -44,55 +44,36 @@ int	make_d(t_task *input, va_list *ap)
 	int len;
 	int len_num;
 
-	(void)input;
 	target = va_arg(*ap, int);
 	ft_baseitoasign(str, target, 10);
 
 	len_num = length_utf8(str);
 	len = (input->precision > len_num) ? input->precision : len_num;
 
-	if (input->width > len)
+	if (input->minus)
 	{
-		if (input->minus)
-		{
-			putsign(input, target);
-			putpres(str, len, len_num);
-			fillsign(input, target, ' ', len);
-		}
-		else
-		{
-			if (input->zero != ' ')
-			{
-				if (input->precision == -1)
-				{
-					putsign(input, target);
-					fillsign(input, target, '0', len);
-					putpres(str, len, len_num);
-				}
-				else
-				{
-					fillsign(input, target, ' ', len);
-					putsign(input, target);
-					putpres(str, len, len_num);
-				}
-			}
-			else
-			{
-				fillsign(input, target, ' ', len);
-				putsign(input, target);
-				putpres(str, len, len_num);
-			}
-		}
+		putsign(input, target);
+		putpreci(str, len, len_num);
+		fillsign(input, target, ' ', len);
 	}
 	else
 	{
-		putsign(input, target);
-		putpres(str, len, len_num);
+		if (input->zero != ' ' && input->precision == -1)
+		{
+			putsign(input, target);
+			fillsign(input, target, '0', len);
+			putpreci(str, len, len_num);
+		}
+		else
+		{
+			fillsign(input, target, ' ', len);
+			putsign(input, target);
+			putpreci(str, len, len_num);
+		}
 	}
 	if (input->plus || input->space || target < 0)
 		len++;
 	return (input->width > len) ? input->width : len;
-
 
 	return (1);
 }
